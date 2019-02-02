879f372500d8f6641be13b8a0e9760e88f081c25
Anna <piladzisi@gmail.com>
2019-02-02 16:58:55 +0200

    change on checkbox

--- 1-adding-and-removing-names/1-tour-the-application/start/app.js
+++ 1-adding-and-removing-names/1-tour-the-application/start/app.js
@@ -1,12 +1,23 @@
 const form = document.getElementById('registrar');
 const input = form.querySelector('input');
+const ul = document.getElementById('invitedList');
 
 form.addEventListener('submit', (event) => {
 	event.preventDefault();
 	const text = input.value;
 	input.value='';
-	const ul = document.getElementById('invitedList');
 	const li = document.createElement('li');
 	li.textContent = text;
+	const label = document.createElement('label');
+	label.textContent = 'Confirmed';
+	const checkBox = document.createElement('input');
+	checkBox.type = 'checkbox';
+	li.appendChild(label);
+	label.appendChild(checkBox);
 	ul.appendChild(li);
 });
+
+ul.addEventListener('change', (event) => {
+	console.log(event.target.checked);
+});
+
